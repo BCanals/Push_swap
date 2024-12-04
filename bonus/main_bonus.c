4467ef8c0261639c19efd4e5d84cb6a82206d73e
BCanals <bizcru@DESKTOP-SAU74E1>
2024-12-04 19:50:27 +0100

    full working bonus :)

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bizcru <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 11:50:33 by bizcru            #+#    #+#             */
-/*   Updated: 2024/12/04 19:22:55 by bizcru           ###   ########.fr       */
+/*   Updated: 2024/12/04 19:44:58 by bizcru           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,23 @@
 
 int	main(int argc, char **argv)
 {
-	t_node	**stack_a;
-	t_node	**stack_b;
+	t_node	*stack_a;
+	t_node	*stack_b;
 
 	stack_b = NULL;
 	if (argc == 1)
 		return (0);
-	*stack_a = data_loader(argc, argv);
-	if (!*stack_a)
+	stack_a = data_loader(argc, argv);
+	if (!stack_a)
 		return (ft_putstr_fd("Error\n", 2));
-	if (!sort_serf(stack_a, stack_b)
+	if (!sort_serf(&stack_a, &stack_b))
 		return (ft_putstr_fd("Error\n", 2));
-	if (!stack_b && is_sorted(*stack_a))
+	if (!stack_b && is_sorted(stack_a))
 		ft_printf("OK\n");
 	else
 		ft_printf("KO\n");
 	if (stack_b)
-		ft_lstclear(stack_b);
-	ft_lstclear(stack_a);
+		ft_lstclear(&stack_b);
+	ft_lstclear(&stack_a);
 	return (0);
 }
